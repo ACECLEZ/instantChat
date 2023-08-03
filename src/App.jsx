32517925120def997e8067c45f946f3c3fc6c758
ACECLEZ <121684067+ACECLEZ@users.noreply.github.com>
2023-08-03 12:08:54 +0800

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AuthProvider } from "./context/AuthContext";
 import ChatRoom from "./pages/ChatRoom";
 import Login from "./pages/Login";
 import { PrivateRoute } from "./routes/PrivateRoute";
-/*
+
 function App() {
   return (
     <AuthProvider>
@@ -13,25 +13,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route
           path="/chat"
-          element={
-            <PrivateRoute>
-              <ChatRoom />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </AuthProvider>
-  );
-}
-*/
-function App() {
-  return (
-    <AuthProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/"
           element={
             <PrivateRoute>
               <ChatRoom />
