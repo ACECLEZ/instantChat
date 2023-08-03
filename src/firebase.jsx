7f5acb2d981aa261ea437d0f3a742dbcbd21d5b1
ACECLEZ <121684067+ACECLEZ@users.noreply.github.com>
2023-08-03 11:39:54 +0800

    init

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+/*
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -10,6 +11,19 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID
 };
+*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMDIcXRy_95DrzD9kSo_GxJl96de4aGO4",
+  authDomain: "chatpwc-711a0.firebaseapp.com",
+  databaseURL: "https://chatpwc-711a0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chatpwc-711a0",
+  storageBucket: "chatpwc-711a0.appspot.com",
+  messagingSenderId: "267904903784",
+  appId: "1:267904903784:web:512e8f032891ddd09869dd",
+  measurementId: "G-V98KGS7GSQ"
+};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
