2d4baaa84a2ed127ceb5bf30b2e654ac049af109
ACECLEZ <121684067+ACECLEZ@users.noreply.github.com>
2023-08-03 12:06:54 +0800

    debug

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
-
+/*
 export const PrivateRoute = ({ children }) => {
   const { currentUser } = UserAuth();
 
@@ -9,3 +9,4 @@ export const PrivateRoute = ({ children }) => {
   }
   return children;
 }
+*/
