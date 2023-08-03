e3f1577a6669b6fa3a417eefdce46f4856a2d44f
ACECLEZ <121684067+ACECLEZ@users.noreply.github.com>
2023-08-03 12:02:44 +0800

    update disclaimer

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,9 +25,12 @@ const Login = () => {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">欢迎来到</h1>
-          <h1 className="text-5xl font-bold">你问我答 - 神兽论坛 👋🏻</h1>
+          <h1 className="text-5xl font-bold">你问我答 - 神兽论坛 🎉🎉🎉</h1>
           <p className="py-6">
           加入论谈，发表你对神兽的独特见解，互相帮助解答疑问:D
+          </p>
+          <p className="py-6">
+          🚨讨论区禁止任何挑衅、粗俗、煽动情绪的字眼和内容，也禁止恶意中伤、诽谤和人身攻击的言论。
           </p>
           <button onClick={handleLogin} className="btn">Google 登录</button>
         </div>
