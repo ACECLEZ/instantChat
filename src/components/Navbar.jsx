4d15a9d5e3e458cfa114fbd6e8c626fe7beda4cb
ACECLEZ <121684067+ACECLEZ@users.noreply.github.com>
2023-08-03 11:56:27 +0800

    simplify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <div className="navbar fixed z-10 bg-neutral text-neutral-content">
       <div className="containerWrap flex justify-between">
         <a className="btn btn-ghost normal-case text-xl">唤醒神兽 - 神兽论坛</a>
-        {currentUser ? <button onClick={handleLogout}>Logout</button> : ""}
+        {currentUser ? <button onClick={handleLogout}>登出账号</button> : ""}
       </div>
     </div>
   )
